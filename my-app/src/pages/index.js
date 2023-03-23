b610df433a2d817f365e79e1e61c625ad011597d
Walter Liendo <walter.liendo@gmail.com>
2023-03-23 20:33:06 -0300

    agregado de boton

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Button from './_button'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -38,7 +39,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-
+        <Button></Button>
         <div className={styles.center}>
           <Image
             className={styles.logo}
